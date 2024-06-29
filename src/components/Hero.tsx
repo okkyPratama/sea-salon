83fc89cb6e76936121364211dbd2cd4d8f98f843
okky-dev <nicholas.tantra@is-gs.com>
2024-06-29 10:36:39 +0700

    add readme files

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -44,30 +44,34 @@ const Hero: React.FC = () => {
 
   return (
     <section className="bg-slate-100">
-      <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
-        <div className="mr-auto place-self-center lg:col-span-7">
-          <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-cyan-600">
-            Beauty and Elegance Redefined
-          </h1>
-          <p className="max-w-2xl mb-6 font-light text-gray-600 lg:mb-8 md:text-lg lg:text-xl ">
-            A sanctuary where you can indulge in the latest hair trends,
-            cutting-edge treatments, and unparalleled pampering.
-          </p>
-          <button
-            onClick={handleBookNowClick}
-            className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-cyan-700 hover:bg-cyan-800 focus:ring-4 focus:ring-primary-300"
-          >
-            Book Now
-            <BookNowIcon />
-          </button>
-        </div>
-        <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
-          <img src="/src/assets/sea-salon-model2-bgremove.png" alt="mockup" />
-        </div>
+    <div className="flex flex-col lg:grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+      <div className="order-2 lg:order-1 mr-auto place-self-center lg:col-span-7">
+        <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-cyan-600">
+          Beauty and Elegance Redefined
+        </h1>
+        <p className="max-w-2xl mb-6 font-light text-gray-600 lg:mb-8 md:text-lg lg:text-xl ">
+          A sanctuary where you can indulge in the latest hair trends,
+          cutting-edge treatments, and unparalleled pampering.
+        </p>
+        <button
+          onClick={handleBookNowClick}
+          className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-cyan-700 hover:bg-cyan-800 focus:ring-4 focus:ring-primary-300"
+        >
+          Book Now
+          <BookNowIcon />
+        </button>
       </div>
-      <ReservationForm isOpen={showReservationForm} onClose={handleCloseForm} />
-      {showLoginForm && !user && <LoginForm onClose={handleCloseForm} />}
-    </section>
+      <div className="order-1 lg:order-2 mb-8 lg:mb-0 lg:mt-0 lg:col-span-5 flex justify-center lg:justify-end">
+        <img 
+          src="/src/assets/sea-salon-model2-bgremove.png" 
+          alt="mockup" 
+          className="max-w-full h-auto"
+        />
+      </div>
+    </div>
+    <ReservationForm isOpen={showReservationForm} onClose={handleCloseForm} />
+    {showLoginForm && !user && <LoginForm onClose={handleCloseForm} />}
+  </section>
   );
 };
 
